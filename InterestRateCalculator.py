2642e230a0df3c8a32d661fd106a55ae6085e1f8
Glen Ogilvie <nelg@linuxsolutions.co.nz>
2023-04-25 13:29:02 +1200

    Tool to calculate PIR tax vs normal tax, allowing you to see what the interest rate needs to be for non-pir investments to return the same or better than a PIR investment

---
+++ InterestRateCalculator.py
@@ -0,0 +1,128 @@
+#!/bin/env python3
+import argparse
+from tabulate import tabulate
+from terminalcolor import ctext, cprint
+
+
+def calculate_after_tax_return(principal, interest_rate, tax_rate):
+    return (principal * interest_rate) * (1 - tax_rate)
+
+
+def calculate_interest_rate_from_after_tax_return(principal, after_tax_return, tax_rate):
+    total_return_before_taxes = after_tax_return / (1 - tax_rate)
+    return (total_return_before_taxes / principal) 
+
+
+def parse_interest_rates(interest_rates):
+    parsed_rates = []
+    for rate in interest_rates:
+        if rate > 1:
+            rate /= 100
+        parsed_rates.append(rate)
+    return sorted(parsed_rates)
+
+def parse_input_percentage(input):
+   if input > 1:
+      return input / 100
+   return input
+
+def hex_to_RGB(hex):
+  ''' "#FFFFFF" -> [255,255,255] '''
+  # Pass 16 to the integer function for change of base
+  return [int(hex[i:i+2], 16) for i in range(1,6,2)]
+
+
+def RGB_to_hex(RGB):
+  ''' [255,255,255] -> "#FFFFFF" '''
+  # Components need to be integers for hex to make sense
+  RGB = [int(x) for x in RGB]
+  return "#"+"".join(["0{0:x}".format(v) if v < 16 else
+            "{0:x}".format(v) for v in RGB])
+
+def create_color_string(rgb):
+    return f"24bit-{rgb[0]};{rgb[1]};{rgb[2]}"
+
+def linear_gradient(start_hex, finish_hex="#FFFFFF", n=10):
+  ''' returns a gradient list of (n) colors between
+    two hex colors. start_hex and finish_hex
+    should be the full six-digit color string,
+    inlcuding the number sign ("#FFFFFF") '''
+  # Starting and ending colors in RGB form
+  s = hex_to_RGB(start_hex)
+  f = hex_to_RGB(finish_hex)
+  # Initilize a list of the output colors with the starting color
+  RGB_list = [create_color_string(s)]
+  # Calcuate a color at each evenly spaced value of t from 1 to n
+  for t in range(1, n):
+    # Interpolate RGB vector for color at the current value of t
+    curr_vector = [
+      int(s[j] + (float(t)/(n-1))*(f[j]-s[j]))
+      for j in range(3)
+    ]
+    # Add it to our list of output colors
+    RGB_list.append(create_color_string(curr_vector))
+
+  return RGB_list
+
+
+parser = argparse.ArgumentParser(description='Calculate the after-tax return on investment at various interest rates.')
+parser.add_argument('--principal', type=float, default=100000, help='the principal amount (default: %(default)s)')
+parser.add_argument('--pir-tax-rate', dest='pir_tax_rate', type=float, default=0.28, help='the tax rate for the portfolio investment income (default: %(default)s)')
+parser.add_argument('--personal-tax-rate', type=float, default=0.33, help='the personal tax rate for the after-tax return (default: %(default)s)')
+parser.add_argument('interest_rates', nargs='*', type=float, default=[0.04, 0.05, 0.052, 0.055, 0.056, 0.058, 0.06, 0.07, 0.08, 0.09, 0.10, 0.11], help='interest rates')
+args = parser.parse_args()
+
+
+
+
+
+principal = args.principal
+pir_tax_rate = parse_input_percentage(args.pir_tax_rate)
+personal_tax_rate = parse_input_percentage(args.personal_tax_rate)
+interest_rates = parse_interest_rates(args.interest_rates)
+
+print(ctext(f"-------- INPUTS --------- ",color="24bit-213;94;245", text_type="bold"))
+print(ctext(f"Principal: $ {principal:.2f}",color="24bit-253;74;215", text_type="bold"))
+print(ctext(f"PIR Tax Rate: {pir_tax_rate:.2%}",color="24bit-253;74;225", text_type="bold"))
+print(ctext(f"Personal Tax Rate: {personal_tax_rate:.2%}",color="24bit-253;74;255", text_type="bold"))    
+print()
+
+brightness = 230
+gradient_steps = int(brightness / len(interest_rates))
+linear_gradient_tax = iter(linear_gradient("#6aa3ff", "#a4ffe4",  len(interest_rates)))
+linear_gradient_interest = iter(linear_gradient("#fbff19", "#ffb99e",  len(interest_rates)))
+linear_gradient_return_before_tax = iter(linear_gradient("#ff3884", "#f8a7ff",  len(interest_rates)))
+
+results = []
+for interest_rate in interest_rates:
+    return_before_taxes = principal * interest_rate
+    tax_payable = return_before_taxes * pir_tax_rate
+    after_tax_return = return_before_taxes - tax_payable
+
+    interest_rate_from_after_tax_return = calculate_interest_rate_from_after_tax_return(principal, after_tax_return, personal_tax_rate)
+    return_before_taxes_using_interest_rate_from_after_tax = principal * interest_rate_from_after_tax_return
+    tax_payable_using_personal_tax_rate = return_before_taxes_using_interest_rate_from_after_tax * personal_tax_rate    
+    tax_color = next(linear_gradient_tax)
+    return_before_tax_color = next(linear_gradient_return_before_tax)
+    results.append([
+        ctext(f"{interest_rate:.2%}", color=f"24bit-255;{brightness};{brightness}"),
+        ctext(f"$ {return_before_taxes:.2f}", color=return_before_tax_color),
+        ctext(f"$ {tax_payable:.2f}", color=tax_color),
+        ctext(f"{interest_rate_from_after_tax_return:.2%}", color=next(linear_gradient_interest)),
+        ctext(f"$ {return_before_taxes_using_interest_rate_from_after_tax:.2f}", color=return_before_tax_color),
+        ctext(f"$ {tax_payable_using_personal_tax_rate:.2f}", color=tax_color),
+        ctext(f"$ {after_tax_return:.2f}", color=f"24bit-{brightness};255;{brightness}")
+    ])
+    brightness -= gradient_steps
+
+headers = [
+     ctext('Interest Rate',color="24bit-53;234;255", text_type="bold"),
+     ctext('Return Before Taxes\n(using input interest rate)',color="24bit-53;234;255", text_type="bold"),
+     ctext('Tax Payable\n(using PIR tax rate)',color="24bit-53;234;255", text_type="bold"),
+     ctext('Interest Rate\n(from After-tax Return)',color="24bit-53;234;255", text_type="bold"),
+     ctext('Return Before Taxes\n(using calculated interest rate)',color="24bit-53;234;255", text_type="bold"),
+     ctext('Tax Payable\n(using personal tax rate)',color="24bit-53;234;255", text_type="bold"),
+     ctext('After-tax Return',color="24bit-53;234;255", text_type="bold")
+]
+
+print(tabulate(results, headers=headers, tablefmt='simple'))
---
+++ README.md
@@ -0,0 +1,46 @@
+This tools is for calculating the after-tax return and various other related values for a given principal amount and a set of interest rates, personal tax rate, and portfolio investment income tax rate. It uses the argparse library to parse command line arguments for the principal, pir_tax_rate, personal_tax_rate, and interest_rates. It then iterates over the interest rates, calculates various values for each rate, and appends the results to a list. Finally, it prints the results in a table using the tabulate library, with various values colored using the terminalcolor library.
+
+The code also includes some helper functions for converting between RGB and hex color codes and generating color gradients.
+
+
+# Requirements:
+pip install -r requirements.txt -t .
+
+# Example output:
+
+-------- INPUTS --------- 
+Principal: $ 100000.00
+PIR Tax Rate: 28.00%
+Personal Tax Rate: 33.00%
+
+Interest Rate    Return Before Taxes            Tax Payable             Interest Rate              Return Before Taxes                 Tax Payable                  After-tax Return
+                 (using input interest rate)    (using PIR tax rate)    (from After-tax Return)    (using calculated interest rate)    (using personal tax rate)
+---------------  -----------------------------  ----------------------  -------------------------  ----------------------------------  ---------------------------  ------------------
+4.00%            $ 4000.00                      $ 1120.00               4.30%                      $ 4298.51                           $ 1418.51                    $ 2880.00
+5.00%            $ 5000.00                      $ 1400.00               5.37%                      $ 5373.13                           $ 1773.13                    $ 3600.00
+5.20%            $ 5200.00                      $ 1456.00               5.59%                      $ 5588.06                           $ 1844.06                    $ 3744.00
+5.50%            $ 5500.00                      $ 1540.00               5.91%                      $ 5910.45                           $ 1950.45                    $ 3960.00
+5.60%            $ 5600.00                      $ 1568.00               6.02%                      $ 6017.91                           $ 1985.91                    $ 4032.00
+5.80%            $ 5800.00                      $ 1624.00               6.23%                      $ 6232.84                           $ 2056.84                    $ 4176.00
+6.00%            $ 6000.00                      $ 1680.00               6.45%                      $ 6447.76                           $ 2127.76                    $ 4320.00
+7.00%            $ 7000.00                      $ 1960.00               7.52%                      $ 7522.39                           $ 2482.39                    $ 5040.00
+8.00%            $ 8000.00                      $ 2240.00               8.60%                      $ 8597.01                           $ 2837.01                    $ 5760.00
+9.00%            $ 9000.00                      $ 2520.00               9.67%                      $ 9671.64                           $ 3191.64                    $ 6480.00
+10.00%           $ 10000.00                     $ 2800.00               10.75%                     $ 10746.27                          $ 3546.27                    $ 7200.00
+11.00%           $ 11000.00                     $ 3080.00               11.82%                     $ 11820.90                          $ 3900.90                    $ 7920.00
+
+
+
+# Example use:
+
+python3 InterestRateCalculator.py --principal 50000 0.06 0.07 
+
+seq 1 .2 40 | xargs python3 InterestRateCalculator.py
+
+## lolcat output
+./InterestRateCalculator.py  | lolcat
+
+
+## HTML output:
+yay python-ansi2html
+python3 InterestRateCalculator.py --principal 500000 4 4.2 4.4 4.6 4.7 4.8 30 33 | ansi2html  > InterestRates.html
